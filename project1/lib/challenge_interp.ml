2d2f6a0a0d8b534de577cd1107430c5bf7597edb
Caroline Tracy <carolinerose207@gmail.com>
2026-02-17 15:44:25 -0500

    Continued to work on challenge_interp.ml

--- project1/lib/challenge_interp.ml
+++ project1/lib/challenge_interp.ml
@@ -233,12 +233,11 @@ let rec eval (rho : Env.t) (e : Ast.Expr.t) : Value.t =
 
     
 
-      When a function is fully evaluated, its bound_so_far (that records the values bound to the parameters so far) goes back to empty, and its parameters equal the vars in bound_so_far
       I SHOULD EVALUATE IT LIKE THIS: f 3 4 5 6 IS ((((f 3) 4) 5) 6). SO LIKE EVALUATE IT ONE BY ONE. 
 
   | Ast.Expr.Fun (param_l, e') -> Value.V_Fun ()
 
-    FIND A WAY TO GET THE BOUND_SO_FAR...
+    FIND A WAY TO GET THE BOUND_SO_FAR... maybe search the param_l list and then do lookup? but i'd have to do a variation of the lookup function that doesn't return an error
 
     Evaluate with rho? I don't think so
     PROBABLY WILL BE SIMILAR TO CALL EXPRESSIONS?
